a8529f37c7b737f029e3c9dce2e8b51b64b77c9a
7aleobe7labrod7 <ps.alabbe@gmail.com>
2025-06-02 19:38:12 -0400

    actualizacion de archivos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @services = Service.limit(3)
+    @tutorials = Tutorial.limit(3)
+    @prompts = Prompt.limit(3)
+    @posts = Post.order(created_at: :desc).limit(3)
   end
 end
